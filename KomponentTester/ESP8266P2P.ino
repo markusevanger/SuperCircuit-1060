6bf3a080c55cb17a95395ccaaef8b6ae9b1e7066
Erlend Joa <102193887+erlendjoa@users.noreply.github.com>
2023-05-15 11:42:10 +0200

    Update ESP8266P2P.ino

--- KomponentTester/ESP8266P2P.ino
+++ KomponentTester/ESP8266P2P.ino
@@ -1,62 +1,20 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-two-way-communication-esp8266-nodemcu/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
+// MAC Addresse til receiver 
+uint8_t broadcastAddress[] = {0xE0, 0x98, 0x06, 0x05, 0xED, 0xC6};
+//E0:98:06:05:ED:C6 = MED FLEKK
+//10:52:1C:E5:51:9F = UTEN FLEKk
 
-// REPLACE WITH THE MAC Address of your receiver 
-uint8_t broadcastAddress[] = {0xE0, 0x98, 0x06, 0x05, 0xED, 0xC5};
-//E0:98:06:05:ED:C6
-//10:52:1C:E5:51:9F
 
-// Digital pin connected to the DHT sensor
-#define DHTPIN 5    
+// Definer en data struktur
+typedef struct structMessage {
+  bool startSignal = true;  // signal som sendes for å starte klokke.
+};
+// Lag et strukturert objekt
+structMessage myData;
+bool startSignalAlone;
 
-// Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
-#define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
-
-DHT dht(DHTPIN, DHTTYPE);
-
-// Define variables to store DHT readings to be sent
-float temperature;
-float humidity;
-
-// Define variables to store incoming readings
-float incomingTemp;
-float incomingHum;
-
-// Updates DHT readings every 10 seconds
-const long interval = 10000; 
-unsigned long previousMillis = 0;    // will store last time DHT was updated 
-
-// Variable to store if sending data was successful
-String success;
-
-//Structure example to send data
-//Must match the receiver structure
-typedef struct struct_message {
-    float temp;
-    float hum;
-} struct_message;
-
-// Create a struct_message called DHTReadings to hold sensor readings
-struct_message DHTReadings;
-
-// Create a struct_message to hold incoming sensor readings
-struct_message incomingReadings;
 
 // Callback when data is sent
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
@@ -71,50 +29,18 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
 
 // Callback when data is received
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
-  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("Bytes received: ");
   Serial.println(len);
-  incomingTemp = incomingReadings.temp;
-  incomingHum = incomingReadings.hum;
-}
-
-void getReadings(){
-  // Read Temperature
-  temperature = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  //float t = dht.readTemperature(true);
-  if (isnan(temperature)){
-    Serial.println("Failed to read from DHT");
-    temperature = 0.0;
-  }
-  humidity = dht.readHumidity();
-  if (isnan(humidity)){
-    Serial.println("Failed to read from DHT");
-    humidity = 0.0;
-  }
-}
-
-void printIncomingReadings(){
-  // Display Readings in Serial Monitor
-  Serial.println("INCOMING READINGS");
-  Serial.print("Temperature: ");
-  Serial.print(incomingTemp);
-  Serial.println(" ºC");
-  Serial.print("Humidity: ");
-  Serial.print(incomingHum);
-  Serial.println(" %");
-}
+} 
  
 void setup() {
   // Init Serial Monitor
   Serial.begin(9600); // 115200 originalt.
 
-  // Init DHT sensor
-  dht.begin();
- 
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
+  //WiFi.disconnect(); comment out???
 
   // Init ESP-NOW
   if (esp_now_init() != 0) {
@@ -133,26 +59,22 @@ void setup() {
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
   
   // Register for a callback function that will be called when data is received
-  esp_now_register_recv_cb(OnDataRecv);
+  // esp_now_register_recv_cb(OnDataRecv);
 }
  
 void loop() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you updated the DHT values
-    previousMillis = currentMillis;
-
-    //Get DHT readings
-    getReadings();
-
-    //Set values to send
-    DHTReadings.temp = temperature;
-    DHTReadings.hum = humidity;
+  esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  delay(1000);
+  /*
+  if (digitalRead(D4) == HIGH) {
+    myData.startSignal = true;
 
     // Send message via ESP-NOW
-    esp_now_send(broadcastAddress, (uint8_t *) &DHTReadings, sizeof(DHTReadings));
+    esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
 
     // Print incoming readings
-    printIncomingReadings();
-  }
+    //printIncomingReadings();
+    Serial.println("=== SENT? ===");
+    delay(1000);  
+  }*/
 }
